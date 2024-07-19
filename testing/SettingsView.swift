9c9ffc79869118b556dd39903cd6ad6df14a5f7a
razorback4417 <razorback4417.800@gmail.com>
2024-07-18 19:29:23 -0700

    ui for settings, baseline for create setup

---
+++ testing/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  testing
+//
+//  Created by Theo L on 7/18/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
