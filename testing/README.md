c6784adf333f0be212aa0f3f2968f8bd9bf36210
razorback4417 <razorback4417.800@gmail.com>
2024-08-03 12:40:50 -0700

    location prediction for create

---
+++ testing/README.md
@@ -0,0 +1,9 @@
+#  Tag
+A social rideshare platform with sustainable rewards.
+
+Tag allows you to find other students going to the same place at the same time. Students will gain 3 main things out of Tag:
+1) cheaper Lyft/Ubers
+2) easy and automated payment splitting with people you share a trip with
+3) discounts/rewards to local resturants for using the app
+
+Tag is set to launch Fall 2024 at UCLA.
--- testing/testingApp.swift
+++ testing/testingApp.swift
@@ -7,12 +7,17 @@
 
 import SwiftUI
 import Firebase
+import GooglePlaces
 
 @main
 struct testingApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject private var userViewModel = UserViewModel()
     @StateObject private var tripViewModel = TripViewModel()
+    
+    init() {
+        GMSPlacesClient.provideAPIKey("AIzaSyBqaKYmm09G9FHCaQfjpsPnpRHEwQT9Ggo")
+    }
     
     var body: some Scene {
         WindowGroup {
