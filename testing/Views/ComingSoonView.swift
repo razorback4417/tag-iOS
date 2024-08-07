60e2066c5b3e29923991732ac660df243fed8b2d
razorback4417 <razorback4417.800@gmail.com>
2024-08-07 00:41:38 -0700

    Bug Fixes - spacing header correction in SettingsView - ride history includes host and joined user - "Login here" styling edit and navigation back to Login page working - Search page greets user by correct first name - Search page profile picture correction - Search page settings button navigation fix - Created trips clear when time pasts - "Can't find a trip? Create one here" styling and fix

---
+++ testing/Views/ComingSoonView.swift
@@ -0,0 +1,18 @@
+//
+//  ComingSoonView.swift
+//  testing
+//
+//  Created by Theo L on 8/5/24.
+//
+
+import SwiftUI
+
+struct ComingSoonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ComingSoonView()
+}
